57e58e1351304823a4ea854673ef24b80023b00e
stereoF <firmamentsess@gmail.com>
2022-09-02 09:00:49 +0800

    change frontend

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,14 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import App from './App.vue'
+import router from './router'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
