686543fdb850e7b7ad53b556f51378f262c80a99
stereoF <firmamentsess@gmail.com>
2022-09-13 00:05:53 +0800

    add dapp interaction

---
+++ frontend/src/components/PushPrePrint.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="hello">
+      <button id="contractCall" @click="contractCall">get the contract owner address</button>
+    </div>
+  </template>
+  
+  <script>
+    import { ethers } from "ethers";
+    import PrePrintArtifact from "../contracts/PrePrintTrack.json";
+    import contractAddress from "../contracts/contract-address.json";
+  
+    export default {
+      methods: {
+        contractCall: async function () {
+          const provider = new ethers.providers.Web3Provider(window.ethereum);
+          const prePrint = new ethers.Contract(
+            contractAddress.PrePrintTrack,
+            PrePrintArtifact.abi,
+            provider
+          );
+          const ownerAddress = await prePrint.owner();
+          console.log(ownerAddress.toString());
+        },
+      },
+      name: "PushPrePrint",
+      props: {
+
+      },
+      async mounted() {
+
+      },
+    };
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+    h1 {
+      margin-top: 24px;
+    }
+  
+    h3 {
+      margin: 40px 0 0;
+    }
+  
+    ul {
+      list-style-type: none;
+      padding: 0;
+    }
+  
+    li {
+      display: inline-block;
+      margin: 0 10px;
+    }
+  
+    a {
+      color: #42b983;
+    }
+  </style>
+  
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
+import Dapp from './Dapp.vue'
 import router from './router'
 
 import './assets/main.css'
 
-const app = createApp(App)
+const app = createApp(Dapp)
 
 app.use(createPinia())
 app.use(router)
