ab483db4f1689e4103b6cb05c9b9e328eda2ccfb
stereoF <firmamentsess@gmail.com>
2023-01-23 18:36:21 +0800

    ignore some sol files in config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox");
 require('dotenv').config()
+const { TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS } = require("hardhat/builtin-tasks/task-names");
+const path = require("path");
 
 const { ALCHEMY_API_KEY, GOERLI_PRIVATE_KEY } = process.env;
+
+subtask(
+  TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS,
+  async (_, { config }, runSuper) => {
+    const paths = await runSuper();
+
+    return paths
+      .filter(solidityFilePath => {
+        const relativePath = path.relative(config.paths.sources, solidityFilePath)
+
+        return relativePath !== "DeSciPrintFactory.sol";
+      })
+  }
+);
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
