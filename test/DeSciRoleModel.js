2d1a6c099fff37e0505f0a18ca0622c9f2547562
stereoF <firmamentsess@gmail.com>
2023-01-24 00:11:58 +0800

    add test of DeSciRoleModel

--- test/DeSciRoleModel.js
+++ test/DeSciRoleModel.js
@@ -6,11 +6,11 @@ const { ethers } = require("hardhat");
 describe("DeSciRoleModel contract", function () {
 
   async function deployDeSciRoleModelFixture() {
-    const [owner, address2] = await ethers.getSigners();
+    const [owner, address2, admin, editor, reviewer] = await ethers.getSigners();
     const DeSciRoleModel = await ethers.getContractFactory("DeSciRoleModel");
     const hardhatDeSciRoleModel = await DeSciRoleModel.deploy();
 
-    return { hardhatDeSciRoleModel, owner, address2 }
+    return { hardhatDeSciRoleModel, owner, address2, admin, editor, reviewer }
   }
 
   describe("Deployment", function() {
@@ -21,6 +21,29 @@ describe("DeSciRoleModel contract", function () {
 
       expect(owner.address).to.equal(await hardhatDeSciRoleModel.owner());
     });
+  });
+
+  describe("Management", function() {
+    it("transfer the ownership", async function () {
+      const { hardhatDeSciRoleModel, address2 } = await loadFixture(
+        deployDeSciRoleModelFixture
+      );
+
+      await hardhatDeSciRoleModel.transferOwnership(address2.address);
+      expect(address2.address).to.equal(await hardhatDeSciRoleModel.owner());
+
+    });
+
+    it("assign the admin", async function () {
+        const { hardhatDeSciRoleModel, admin } = await loadFixture(
+          deployDeSciRoleModelFixture
+        );
+  
+        await hardhatDeSciRoleModel.assignAdministrator(admin.address);
+        expect(admin.address).to.equal((await hardhatDeSciRoleModel.admins())[0]);
+        expect(await hardhatDeSciRoleModel.connect(admin).isAdmin()).to.equal(true);
+  
+      });
   });
 
 });
