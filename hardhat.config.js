c5ebb53b787384c58453d0cacf0ffe47ba79d21e
stereoF <firmamentsess@gmail.com>
2023-03-25 01:48:27 +0800

    add ganache network

--- hardhat.config.js
+++ hardhat.config.js
@@ -35,6 +35,10 @@ module.exports = {
     hardhat: {
       chainId: 1337 // We set 1337 to make interacting with MetaMask simpler
     },
+    ganache: {
+      url: "http://172.28.160.1:7545",
+      accounts: ["0x5133ea3942063492ef6bfdab57c9bc0f820bb57717c13a83c76c3dcd505da4f6"]
+    },
     // goerli: {
     //   url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
     //   accounts: [GOERLI_PRIVATE_KEY]
